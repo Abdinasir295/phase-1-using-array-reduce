aceaec8caa17385f81fce49925658b016d90cc19
Abdinasir295 <abdinasirmehdi62@gmail.com>
2024-04-03 11:59:23 +0300

    updated

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+const assembledBatteries = [
+    { type: 'AA', quantity: 10 },
+    { type: 'AAA', quantity: 12 },
+  ];
+  
+   
+  function reducer(accumulator, currentValue) {
+    return accumulator + currentValue.quantity;
+  }
+  
+  const totalBatteries = assembledBatteries.reduce(reducer, 0);
+  
+  
+  
